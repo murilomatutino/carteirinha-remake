270a6996cb8398109320e5943de4005001e4d5e4
Murilo Matutino <mmatutino617@gmail.com>
2025-03-25 23:35:06 -0300

    Qrcode reader
    - Adicionado leitor de qr code que redireciona para o destino apontado pelo qrcode
    
    Falta:
    - estilizar a pagina

---
+++ View/qr-code-estudante.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+    <title>QR code reader</title>
+</head>
+<body>
+    <div id="reader" width="600px"></div>
+
+    <script type="text/javascript" src="js/read-qrcode.js"></script>
+</body>
+</html>
