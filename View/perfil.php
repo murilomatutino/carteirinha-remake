49363c72b49082136956d49861ee2a629ac5df95
Murilo Matutino <mmatutino617@gmail.com>
2025-03-29 17:32:23 -0300

    Criação da pagina perfil + estilização

---
+++ View/perfil.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/perfil.css">
+    <title>Perfil</title>
+</head>
+<body>
+    <header class="session-1">
+        <a href='https://portal.ifba.edu.br/seabra' target='_blank'>
+            <img class="img-logo" src='assets/1b1210fdf4454600bea220983da0cc63.png' alt='logo-ifba-seabra' draggable='false'>
+        </a>
+    </header>
+
+    <?php include('navbar.php')?>
+
+    <main>
+        <div class="form"> 
+            <h1 class="titulo-principal">Perfil</h1>
+            <div>
+                <label>Nome</label>
+                <div class="input"></div>
+            </div> 
+
+            <div>
+                <label>Matricula</label>
+                <div class="input"></div>
+            </div> 
+
+            <div>
+                <label>Email</label>
+                <div class="input"></div>
+            </div> 
+
+            <div>
+                <label>Telefone</label>
+                <div class="input"></div>
+            </div>
+
+            <a href="#">Mudar senha</a>
+        </div>
+    </main>
+    
+    <?php include('footer.php')?>
+</body>
+</html>
